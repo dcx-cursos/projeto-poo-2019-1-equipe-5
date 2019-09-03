b9cffcdc17e8ec74465bd30b78c99d50d41d44fe
Aisllan Dvirson <aisllangba82@gmail.com>
2019-09-03 10:17:31 -0300

    Delete NumeroJogadoresInsuficienteException.java

--- projeto_banco_imobiliario/exercicios_banco_imobiliario_alternativo/NumeroJogadoresInsuficienteException.java
+++
@@ -1,8 +0,0 @@
-package exercicios_banco_imobiliario_alternativo;
-
-public class NumeroJogadoresInsuficienteException extends Exception {
-
-	public NumeroJogadoresInsuficienteException(String msg) {
-		super(msg);
-	}
-}
