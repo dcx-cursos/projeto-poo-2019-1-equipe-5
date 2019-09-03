03aab2283e51c501e414747edd1abb02966fb553
Aisllan Dvirson <aisllangba82@gmail.com>
2019-09-03 10:17:22 -0300

    Delete NumeroJogadoresExcedenteException.java

--- projeto_banco_imobiliario/exercicios_banco_imobiliario_alternativo/NumeroJogadoresExcedenteException.java
+++
@@ -1,8 +0,0 @@
-package exercicios_banco_imobiliario_alternativo;
-
-public class NumeroJogadoresExcedenteException extends Exception {
-
-	public NumeroJogadoresExcedenteException(String msg) {
-		super(msg);
-	}
-}
